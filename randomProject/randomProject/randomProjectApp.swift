6df91dd4e7ea64d6d7f15ee49cabc489312aa8f9
Julia Vister <92576942+Juliavister@users.noreply.github.com>
2023-03-15 17:03:04 +0100

    Add files via upload

---
+++ randomProject/randomProject/randomProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomProjectApp.swift
+//  randomProject
+//
+//  Created by Julia Vister on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct randomProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() //creates a brand new content view (a new screen)
+        }
+    }
+}
